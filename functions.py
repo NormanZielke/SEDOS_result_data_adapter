c7b32e9de85667ae4190ba6bb17ba3459d83211d
Norman Zielke <norman.zielke@rl-institut.de>
2024-11-16 17:29:53 +0100

    Add function to fill the columns scenario, parameter, input_groups, ouput_groups and value

--- functions.py
+++ functions.py
@@ -1,10 +1,22 @@
 import pandas as pd
 
+# Testet ob im "name" am Ende ein Zahl steht
+def split_name(name):
+    a = name.split('_')
+    if len(a) == 1:
+        return a
+    if len(a) > 1:
+        parts = name.rsplit('_', 1)  # Trennt den String in zwei Teile
+        if parts[1].isdigit():  # Überprüft, ob der letzte Teil eine Zahl ist
+            return parts[0].split('_') + [int(parts[1])]
+        else:
+            return name.split('_')
+
 # Teilt die Einträge in Spalte "name" von data und entsprechende Zuordnung in Spalten von results
 def name_function(data,results,columns):
     for i in data.index:
-        a = data.loc[i, "name"].split('_')
-        #a = split_string(data.loc[i, "name"])
+        a = split_name(data.loc[i, "name"])
+        # verschieden verzweigungen, abhängig von den Einträgen in der Spalte "name"
         if len(a) == 1:
             results.loc[i, columns[2]] = data.loc[i, "name"]
             continue
@@ -24,3 +36,33 @@ def name_function(data,results,columns):
             results.loc[i, columns[4]] = a[0]
             a = a[1:]
             results.loc[i, columns[5]] = a
+
+def var_name_function(data,results):
+    energy_units = ["MWh","kWh","PJ"]
+    weighted_units = ["Mt","Mt/a","kg"]
+    for i in data.index:
+        a = data.loc[i, "var_name"].split('_')
+        # verschieden verzweigungen, abhängig von den Einträgen in der Spalte "var_name"
+        # invest - Verzweigung
+        if a[0] == "invest":
+            if data.loc[i,"unit"] in energy_units:
+                results.loc[i, "parameter"] = "capacity_p_inst"
+                continue
+            if data.loc[i,"unit"] in weighted_units:
+                results.loc[i, "parameter"] = "capacity_w_inst"
+                continue
+        # flow - Verzweigung
+        if a[0] == "flow":
+            results.loc[i, "parameter"] = "flow_volume"
+            if a[1] == "in":
+                a = a[2:]
+                results.loc[i, "input_groups"] = '_'.join(a)
+                continue
+            if a[1] == "out":
+                a = a[2:]
+                results.loc[i, "output_groups"] = '_'.join(a)
+                continue
+        # system - Verzweigung
+        else:
+            results.loc[i, "process"] = '_'.join(a[:2])
+            results.loc[i, "parameter"] = a[-1]
--- result_data_adapter.py
+++ result_data_adapter.py
@@ -1,7 +1,9 @@
 
 from openpyxl import load_workbook
 import pandas as pd
-from functions import name_function
+from functions import name_function, var_name_function
+
+# Vorbereitung Output
 
 # create dataframe for results
 columns = ["scenario", "parameter", "process", "sector", "category",
@@ -9,6 +11,8 @@ columns = ["scenario", "parameter", "process", "sector", "category",
                "output_groups", "unit", "value"]
 
 results = pd.DataFrame(columns= columns)
+
+# Vorbereitung Input
 
 data = pd.read_csv("results.csv",
                         sep=",")
@@ -19,17 +23,44 @@ data['name'] = data['name'].str.replace(r'--\d+$', '', regex=True)
 # Entfernen aller Zeilen, bei denen die Werte in der Spalte "name" mit "helper" beginnen
 data = data[~data['name'].str.startswith('helper')]
 data.reset_index(drop=True)
+# --------------------------------------------------------------------------------------------------------------------->
+
+# Erzeugen von einer test Zeile in data für var_name == invest
+test_data = {"name": ["ind_steel_casting_1","ind_steel_casting_2"],
+        "var_name": ["invest_out_exo_steel","invest_out_exo_steel"],
+        "var_value": [999,888],
+        "region": ["DE","DE"],
+        "type":[{},{}],
+        "carrier": [{},{}],
+        "tech":["tech","tech"],
+        "unit":["MWh","kg"]
+}
+df_test = pd.DataFrame(test_data)
+
+data['unit'] = "MWh"
+
+data2 = pd.concat([data, df_test], ignore_index=True)
+
 
 # --------------------------------------------------------------------------------------------------------------------->
 
-name_function(data,results,columns)
+# fill the results dataframe with data
 
+name_function(data,results,columns) # evtl. columns als argument entfernen
+
+var_name_function(data,results)
+
+results.value = data.var_value
+
+results.scenario = "o_steel_tokio"
 # --------------------------------------------------------------------------------------------------------------------->
-'''
-for i in data.index:
-    a = data.loc[i, "var_name"].split('_')
-    if a[0] == "flow":
-        results.loc[i, "para"]
-        if a[1] == "in":
 
-'''
+# fill the results dataframe with data incl. testdata
+
+name_function(data2,results,columns) # evtl. columns als argument entfernen
+
+var_name_function(data2,results)
+
+results.value = data2.var_value
+
+results.scenario = "o_steel_tokio"
